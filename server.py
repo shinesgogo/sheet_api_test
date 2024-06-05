8c50bd11d6e1e031267ce878d494e7fe5bf6df2d
shinesgogo <shines.techdev@gmail.com>
2024-06-05 19:19:59 +0800

    Update server.py

--- server.py
+++ server.py
@@ -17,10 +17,24 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+credentials_dict = {
+    "type": "service_account",
+    "project_id": os.getenv("PROJECT_ID"),
+    "private_key_id": os.getenv("PRIVATE_KEY_ID"),
+    "private_key": os.getenv("PRIVATE_KEY").replace('\\n', '\n'),
+    "client_email": os.getenv("CLIENT_EMAIL"),
+    "client_id": os.getenv("CLIENT_ID"),
+    "auth_uri": os.getenv("AUTH_URI"),
+    "token_uri": os.getenv("TOKEN_URI"),
+    "auth_provider_x509_cert_url": os.getenv("AUTH_PROVIDER_X509_CERT_URL"),
+    "client_x509_cert_url": os.getenv("CLIENT_X509_CERT_URL"),
+    "universe_domain": "googleapis.com",
+}
+
 
 # Load credentials from the JSON file
 credentials = service_account.Credentials.from_service_account_file(
-    './credentials.json',  # Update with the path to your credentials file
+    credentials_dict,  # Update with the path to your credentials file
     scopes=['https://www.googleapis.com/auth/spreadsheets']
 )
 
